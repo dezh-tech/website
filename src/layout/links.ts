c2f5c48331e459aac2ba001cb190d4588f321c96
Hamed ahamdi <107760095+hamedtkd@users.noreply.github.com>
2024-12-30 20:09:04 +0330

    fix: update links in header (#35)

--- src/layout/links.ts
+++ src/layout/links.ts
@@ -6,11 +6,11 @@ type LinkT = {
 export const links: LinkT[] = [
   {
     name: "Our Partners",
-    link: "our-Partners",
+    link: "#our-Partners",
   },
   {
     name: "Community",
-    link: "community",
+    link: "#community",
   },
   {
     name: "Documents",
