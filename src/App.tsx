9f5afdd93e3ea4bb9dd218c08fe3b4fb57cd0520
Hamed Ahmadi <hamed.raadco@gmail.com>
2025-02-27 10:55:49 +0330

    feat: add redirect link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Header, Footer } from "layout";
 import { ReactLenis } from "lenis/react";
 import Home from "components/Home";
+import RedirectHandler from "components/RedirectHandler";
 import "./App.css";
 
 function App() {
   return (
     <>
       <ReactLenis root>
+        <RedirectHandler />
         <Header />
         <main>
           <Home />
